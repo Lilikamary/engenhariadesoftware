9aecd6051c9f813ee534062ce0c4e70db9b80a8f
Lilian <lilian.goncalves@ifmt.edu.br>
2019-01-31 20:50:35 -0300

    Endereço - colocado a listagem dos resultados.

--- Desenvolvimento/vimperial/src/br/ufpe/cin/vimperial/bean/EnderecoBean.java
+++ Desenvolvimento/vimperial/src/br/ufpe/cin/vimperial/bean/EnderecoBean.java
@@ -1,8 +1,11 @@
 package br.ufpe.cin.vimperial.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import br.ufpe.cin.vimperial.entidades.Endereco;
 import br.ufpe.cin.vimperial.util.JPAUtil;
@@ -31,6 +34,18 @@ public class EnderecoBean {
 		this.endereco = endereco;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public List<Endereco> listarTodos(){
+		
+		EntityManager manager = JPAUtil.getEntityManager();
+		manager.getTransaction().begin();		
+		Query query = manager.createQuery("FROM endereco");
+        List<Endereco> enderecos = query.getResultList();
+        manager.close();
+        return enderecos;
+		
+		
+	}
 	
 	
 }
