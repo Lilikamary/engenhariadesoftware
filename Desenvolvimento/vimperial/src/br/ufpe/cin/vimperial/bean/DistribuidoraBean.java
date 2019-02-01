c356cbea420db7345764f66b217bf205c995cf9e
Lilian <lilian.goncalves@ifmt.edu.br>
2019-02-01 14:20:07 -0300

    Atualização do bean - perda do projeto

--- Desenvolvimento/vimperial/src/br/ufpe/cin/vimperial/bean/DistribuidoraBean.java
+++ Desenvolvimento/vimperial/src/br/ufpe/cin/vimperial/bean/DistribuidoraBean.java
@@ -1,8 +1,11 @@
 package br.ufpe.cin.vimperial.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 
 import br.ufpe.cin.vimperial.entidades.Distribuidora;
@@ -24,6 +27,29 @@ public class DistribuidoraBean {
 		manager.close();
 	}
 
+	
+	public void excluir(Distribuidora distribuidora) {
+        try {
+        	EntityManager manager = JPAUtil.getEntityManager();
+            manager.getTransaction().begin();
+            manager.remove(distribuidora);
+            manager.getTransaction().commit();
+            manager.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+	
+	public Distribuidora localizarDistribuidora(Long codDistribuidora) {
+		
+		EntityManager manager = JPAUtil.getEntityManager();
+		Distribuidora distribuidoraLocalizado = manager.find(Distribuidora.class, codDistribuidora);
+		manager.close();
+		return distribuidoraLocalizado;
+		
+	}
+	
 	public Distribuidora getDistribuidora() {
 		return distribuidora;
 	}
@@ -32,6 +58,18 @@ public class DistribuidoraBean {
 		this.distribuidora = distribuidora;
 	}
 
+	@SuppressWarnings("unchecked")
+	public List<Distribuidora> listarTodos(){
+		
+		EntityManager manager = JPAUtil.getEntityManager();
+		manager.getTransaction().begin();		
+		Query query = manager.createQuery("FROM distribuidora");
+        List<Distribuidora> distribuidoras = query.getResultList();
+        manager.close();
+        return distribuidoras;
+		
+		
+	}
 	
 	
 }
